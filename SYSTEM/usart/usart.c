4b44adf6e83624bf3e832c0de389437af00f93ea
zengbin <zengbin@sensetime.com>
2019-06-21 21:51:09 +0800

    Demo: Can work on my board now

--- SYSTEM/usart/usart.c
+++ SYSTEM/usart/usart.c
@@ -26,7 +26,7 @@
 //1,修改串口初始化IO的bug
 //2,修改了USART_RX_STA,使得串口最大接收字节数为2的14次方
 //3,增加了USART_REC_LEN,用于定义串口最大允许接收的字节数(不大于2的14次方)
-//4,修改了EN_USART1_RX的使能方式
+//4,修改了EN_USART2_RX的使能方式
 //V1.5修改说明
 //1,增加了对UCOSII的支持
 //////////////////////////////////////////////////////////////////////////////////
@@ -52,8 +52,8 @@ void _sys_exit(int x)
 //重定义fputc函数
 int fputc(int ch, FILE *f)
 {
-    while((USART1->SR&0X40)==0);//循环发送,直到发送完毕
-    USART1->DR = (u8) ch;
+    while((USART2->SR&0X40)==0);//循环发送,直到发送完毕
+    USART2->DR = (u8) ch;
     return ch;
 }
 #endif
@@ -62,21 +62,21 @@ int fputc(int ch, FILE *f)
 /*
 int fputc(int ch, FILE *f)
 {
-USART_SendData(USART1, (uint8_t) ch);
+USART_SendData(USART2, (uint8_t) ch);
 
-while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET) {}
+while (USART_GetFlagStatus(USART2, USART_FLAG_TC) == RESET) {}
 
    return ch;
 }
 int GetKey (void)  {
 
-   while (!(USART1->SR & USART_FLAG_RXNE));
+   while (!(USART2->SR & USART_FLAG_RXNE));
 
-   return ((int)(USART1->DR & 0x1FF));
+   return ((int)(USART2->DR & 0x1FF));
 }
 */
 
-#if EN_USART1_RX   //如果使能了接收
+#if EN_USART2_RX   //如果使能了接收
 //串口1中断服务程序
 //注意,读取USARTx->SR能避免莫名其妙的错误
 u8 USART_RX_BUF[USART_REC_LEN];     //接收缓冲,最大USART_REC_LEN个字节.
@@ -94,29 +94,28 @@ void uart_init(u32 bound) {
     USART_InitTypeDef USART_InitStructure;
     NVIC_InitTypeDef NVIC_InitStructure;
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1|RCC_APB2Periph_GPIOA, ENABLE);	//使能USART1，GPIOA时钟
-    USART_DeInit(USART1);  //复位串口1
-    //USART1_TX   PA.9
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9; //PA.9
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO|RCC_APB2Periph_GPIOA, ENABLE);	//使能GPIOA时钟
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);	//使能USART2时钟
+    USART_DeInit(USART2);  //复位串口1
+    //USART2_TX   PA.2
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2; //PA.2
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;	//复用推挽输出
-    GPIO_Init(GPIOA, &GPIO_InitStructure); //初始化PA9
+    GPIO_Init(GPIOA, &GPIO_InitStructure); //初始化PA2
 
-    //USART1_RX	  PA.10
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+    //USART2_RX	  PA.3
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;//浮空输入
-    GPIO_Init(GPIOA, &GPIO_InitStructure);  //初始化PA10
+    GPIO_Init(GPIOA, &GPIO_InitStructure);  //初始化PA3
 
     //Usart1 NVIC 配置
-
-    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=3 ;//抢占优先级3
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;		//子优先级3
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
     NVIC_Init(&NVIC_InitStructure);	//根据指定的参数初始化VIC寄存器
 
     //USART 初始化设置
-
     USART_InitStructure.USART_BaudRate = bound;//一般设置为9600;
     USART_InitStructure.USART_WordLength = USART_WordLength_8b;//字长为8位数据格式
     USART_InitStructure.USART_StopBits = USART_StopBits_1;//一个停止位
@@ -124,21 +123,23 @@ void uart_init(u32 bound) {
     USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//无硬件数据流控制
     USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;	//收发模式
 
-    USART_Init(USART1, &USART_InitStructure); //初始化串口
-    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//开启中断
-    USART_Cmd(USART1, ENABLE);                    //使能串口
+    USART_Init(USART2, &USART_InitStructure); //初始化串口
+    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);//开启中断
+    USART_Cmd(USART2, ENABLE);                    //使能串口
 
 }
 
-void USART1_IRQHandler(void)                	//串口1中断服务程序
+void USART2_IRQHandler(void)                	//串口1中断服务程序
 {
     u8 Res;
 #ifdef OS_TICKS_PER_SEC	 	//如果时钟节拍数定义了,说明要使用ucosII了.
     OSIntEnter();
 #endif
-    if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)  //接收中断(接收到的数据必须是0x0d 0x0a结尾)
+    if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)  //接收中断(接收到的数据必须是0x0d 0x0a结尾)
     {
-        Res =USART_ReceiveData(USART1);//(USART1->DR);	//读取接收到的数据
+        Res =USART_ReceiveData(USART2);//(USART2->DR);	//读取接收到的数据
+        //printf("Debug Res: %x, USART_RX_STA: %x\r\n", Res, USART_RX_STA);
+        //printf("%c", Res);
 
         if((USART_RX_STA&0x8000)==0)//接收未完成
         {
--- USER/main.c
+++ USER/main.c
@@ -9,16 +9,16 @@
 int main(void)
 {
     u16 i=0;
-    IAP_Init();
+    //IAP_Init();
 
     delay_init();	    	 //延时函数初始化
     NVIC_Configuration(); 	 //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
     uart_init(115200);	 	 //串口初始化为9600
     LED_Init();			     //LED端口初始化
-    LCD_Init();	             //初始化LCD
+    //LCD_Init();	             //初始化LCD
     usmart_dev.init(SystemCoreClock/1000000);	//初始化USMART
 
-    printf("\r\n 输入:help查询指令\r\n");
+    printf("\r\n Input help to get more info. \r\n");
     while(1)
     {
         i++;
