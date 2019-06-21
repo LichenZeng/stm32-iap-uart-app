45948e0682b11ec002bcd98e297cf25dd2884aeb
zengbin <zengbin@sensetime.com>
2019-06-21 20:49:53 +0800

    Demo: Just format the code

--- SYSTEM/iap/iap.h
+++ SYSTEM/iap/iap.h
@@ -6,9 +6,9 @@
 #define USE_BKP_SAVE_FLAG     1
 
 #if (USE_BKP_SAVE_FLAG == 1)
-  #define INIT_FLAG_DATA      0x0000  //默认标志的数据(空片子的情况)
+#define INIT_FLAG_DATA      0x0000  //默认标志的数据(空片子的情况)
 #else
-  #define INIT_FLAG_DATA      0xFFFF   //默认标志的数据(空片子的情况)
+#define INIT_FLAG_DATA      0xFFFF   //默认标志的数据(空片子的情况)
 #endif
 #define UPDATE_FLAG_DATA      0xEEEE   //下载标志的数据
 #define UPLOAD_FLAG_DATA      0xDDDD   //上传标志的数据
@@ -16,9 +16,9 @@
 #define APPRUN_FLAG_DATA      0x5A5A   //APP不需要做任何处理，直接运行状态
 
 #if (USE_BKP_SAVE_FLAG == 1)
-  #define IAP_FLAG_ADDR    BKP_DR1
+#define IAP_FLAG_ADDR    BKP_DR1
 #else
-  #define IAP_FLAG_ADDR   (uint32_t)(ApplicationAddress - 1024 * 2)//App区域和Bootloader区域共享信息的地址(暂定大小为2K)
+#define IAP_FLAG_ADDR   (uint32_t)(ApplicationAddress - 1024 * 2)//App区域和Bootloader区域共享信息的地址(暂定大小为2K)
 #endif
 
 #define IAP_FLASH_SIZE  0x3000//Bootloader区域大小
